02ebc933ca2f9395edbeb16275fd792d257b3d78
RaideNnigth <gustavo.pinzon55@gmail.com>
2025-11-30 07:55:50 -0300

    For now no email, soo Ravilon does not get ban befored the picht

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,6 +1,11 @@
 import transporter from "./mailer.js";
 
 export async function EmailService({ to, subject, text, html }) {
+  
+  if (true) {
+    return;
+  }
+
   return transporter.sendMail({
     from: `"Meu Sistema" <${process.env.SMTP_USER}>`,
     to,
